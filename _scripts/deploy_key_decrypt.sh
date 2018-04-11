ffb0442fc095f53c29a8361757e033f38d64cfdc
David Ardoin <dtardoin@eventbrite.com>
2018-04-11 17:17:54 -0400

    something that works

--- _scripts/deploy_key_decrypt.sh
+++ _scripts/deploy_key_decrypt.sh
@@ -1,20 +1,19 @@
 #!/bin/bash 
 # Decrypt Github Deploy key for the Semantic Release package to be able to push tags 
 # Ubuntu-14.04
-
+# Decrypt key 
+/usr/bin/openssl aes-256-cbc -K $encrypted_d09c1c3e25e0_key -iv $encrypted_d09c1c3e25e0_iv -in git_deploy_key.enc -out /tmp/git_deploy_key -d
 # Make sure only the current user can read the private key- 
-/usr/bin/openssl aes-256-cbc -K "${encrypted_ba12a3340263_key}" -iv "${encrypted_ba12a3340263_iv}" -in git_deploy_key.enc -out /tmp/git_deploy_key -d 
-# Make sure only the current user can read the private key
 /bin/chmod 600 /tmp/git_deploy_key
-# Create a script to return the passphrase environment variable to ssh-add
-/bin/echo 'echo ${SSH_PASSPHRASE}' > /tmp/askpass && chmod +x /tmp/askpass
+#Remove passphrase from key 
+/usr/bin/ssh-keygen -p -P "${SSH_PASSPHRASE}" -N "" -f /tmp/git_deploy_key
 # Start the authentication agent
-eval "$(/usr/bin/ssh-agent -s)"
+eval "$(ssh-agent -s)"
 # Add the key to the authentication agent
-DISPLAY=":0.0" SSH_ASKPASS="/tmp/askpass" /usr/bin/setsid ssh-add /tmp/git_deploy_key </dev/null
+/usr/bin/setsid ssh-add /tmp/git_deploy_key 
+
 # semantic-relase #placeholder 
 
-echo -e "Script, and processes spawned:\n$(ps -ef|grep $$)\n\n"
-/usr/bin/pkill ssh-agent && echo "Successfully killed ssh-agent"
-echo -e "All processes running: $(ps -ef)"
-exit 0
+ssh-add -l
+/usr/bin/pkill ssh-agent && echo "successfully killed ssh agent"
+
