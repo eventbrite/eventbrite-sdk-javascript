6957828af3ef5b0bbe5564a29f67295260618330
Jonathan Creamer <matrixhasyou2k4@gmail.com>
2019-01-29 11:27:08 -0600

    feat: add docs generator

--- src/users.ts
+++ src/users.ts
@@ -17,10 +17,17 @@ export interface User {
  * API for working with Users
  */
 export class UserApi extends BaseApi<User> {
+    /**
+     * Get the logged in users info
+     */
     me() {
         return this.request('/users/me/');
     }
 
+    /**
+     * Get user by ID
+     * @param id {string}
+     */
     get(id: string) {
         return this.request(`/users/${id}/`);
     }
